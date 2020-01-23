48b2d73dc8ff41b75d71de4f5b12cb2c18cbca38
Rob W <robpwatkins@gmail.com>
2020-01-23 11:54:46 -0600

    yup

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-document.querySelector('button').addEventListener('click', () => {
+document.querySelectorAll('button').forEach(button => button.addEventListener('click', () => {
   const div = document.createElement('div');
   div.innerHTML = 'Hi there.'
   div.classList.add('clicked');
   document.querySelector('body').appendChild(div);
-})
+}))
