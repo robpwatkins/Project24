686b3b412d8df3ac7f2c86e634d71d6c908f8d8e
Rob W <robpwatkins@gmail.com>
2020-01-23 11:42:51 -0600

    yup

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.querySelector('button').addEventListener('click', () => {
   const div = document.createElement('div');
   div.innerHTML = 'Hi there.'
+  div.style.border = '1px solid black';
   document.querySelector('body').appendChild(div);
 })
